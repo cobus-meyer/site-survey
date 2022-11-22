3a6e9eb5941c5c6033fe738c5ffc5150adc83cf6
Cobus Meyer <cobusmeyer@hotmail.com>
2022-11-22 22:57:28 +0100

    first commit

---
+++ site_survey/lib/main_staging.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:site_survey/app/app.dart';
+import 'package:site_survey/bootstrap.dart';
+
+import 'package:site_survey/firebase_options.dart';
+
+
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await bootstrap(() => const App());
+}
